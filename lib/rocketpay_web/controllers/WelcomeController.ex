7e992d4f088cfccbc5d23c3eeb3fbb29e4c8e3ff
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2021-02-22 09:43:26 -0300

    First Commit

---
+++ lib/rocketpay_web/controllers/WelcomeController.ex
@@ -0,0 +1,9 @@
+defmodule RocketpayWeb.WelcomeController do
+  # Tornamos esse módulo especial dizendo que ele será um controller
+  use RocketpayWeb, :controller
+
+  # Aqui criaremos o index que será renderizado nas rotas
+  def index(){
+
+  }
+end
