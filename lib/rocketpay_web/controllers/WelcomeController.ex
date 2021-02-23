3ae97acecffc971e61f7b31e912a2d2cde7bc7be
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2021-02-22 22:53:07 -0300

    Refactor route

--- lib/rocketpay_web/controllers/WelcomeController.ex
+++ lib/rocketpay_web/controllers/WelcomeController.ex
@@ -2,11 +2,22 @@ defmodule RocketpayWeb.WelcomeController do
   # Tornamos esse módulo especial dizendo que ele será um controller
   use RocketpayWeb, :controller
 
+  alias Rocketpay.Numbers
+
   # Aqui criaremos o index que será renderizado nas rotas
   # Toda função recebe dois parâmetros a conexão e os parametross
-  def index(conn, _params) do
-    text(conn, "Welcome to the Rocketpay API")
+  def index(conn, %{"filename" => filename}) do
+    filename |> Numbers.sum_from_file() |> handle_response(conn)
   end
 
+  defp handle_response({:error, %{result: result}}, conn)do
+    conn |> put_status(:ok)
+    |> json(%{message: "Welcome to Rocketpay API. Here is your number! #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn |> put_status (:bad_request)
+    |> json(reason)
+  end
 
 end
